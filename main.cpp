72c07f71f12345ecde0a4cfed056a6a1d643cfae
Trushkov. I <ivanyatrush@gmail.com>
2026-09-09 23:53:24 +0300

    User interface fixed.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 int main() {
-	std::cout << "Hello world";
+	int h, l;
+	double S;
+
+	std::cout << "Input height of a triangle" << std::endl;
+	std::cin >> h;
+	std::cout << "Input length of the side" << std::endl;
+	std::cin >> l;
+
+	S = h * l * 0.5;
+
+	std::cout << "S = " << S;
 	return 0;
 }
